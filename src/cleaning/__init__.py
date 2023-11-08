62ca8fd880ccffa0c9b2eda09f36c307d61df02d
Lee Young Ho <lkxv999@gmail.com>
2023-11-08 18:42:04 +0800

    encoders added

--- src/cleaning/__init__.py
+++ src/cleaning/__init__.py
@@ -7,7 +7,19 @@ Function:   Init fot Data Cleaning and Preprocessing
 """
 
 from data_preprecess import data_preprocess
+from data_encoding import data_encoding
+import pandas as pd
 
-data = data_preprocess()
-data.to_csv(r'..\..\data\processed\Data_Preprocess_v3.csv',
+# data = data_preprocess()
+# data.to_csv(r'..\..\data\processed\Data_Preprocess_v6.csv',
+#             index=False, header=True, mode='w')
+
+# data = data_encoding(data)
+# data.to_csv(r'..\..\data\processed\Data_Preprocess_Encode_v1.csv',
+#             index=False, header=True, mode='w')
+
+data = pd.read_csv('./data/processed/Data_Preprocess_v6.csv', low_memory=False)
+
+data = data_encoding(data)
+data.to_csv('./data/processed/Data_Preprocess_Encode_v1.csv',
             index=False, header=True, mode='w')
