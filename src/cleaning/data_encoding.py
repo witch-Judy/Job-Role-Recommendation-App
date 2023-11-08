b71701204fa258f8483a1ed2adcd08114e71d5b1
Lee Young Ho <lkxv999@gmail.com>
2023-11-08 18:42:04 +0800

    encoders added

---
+++ src/cleaning/data_encoding.py
@@ -0,0 +1,43 @@
+"""
+Author:     Liao Yueh-Fan
+Data:       2023/11/8
+Course:     NUS IT5006 Fundamentals of Data Analytics
+Project:    Job Role Recommendation App
+Function:   Functions for Data Encoding
+"""
+import pandas as pd
+import data_encoding_constant as constant
+
+def data_encoding(data):
+    # multiple selections question (one question have several columns): change each column's value into 0 and 1
+    for df_col in data.columns.values:
+        if any(substring in df_col for substring in constant.mulSelQuestionTitle):
+            mulSelQuestionEncode(data, df_col)
+
+    # single selection question (one question have one column): remove the column and add one-hot encoding columns
+    data = onehotEncoder(data, constant.sinSelQuestionTitle)
+    
+    return data
+
+def mulSelQuestionEncode(df, col_name):
+    # change the column value that is not blank and null to 1 (ex. Q12: R -> Q12: 1)
+    df[col_name].replace(pd.unique(df[df[col_name].notnull()][col_name]), 1, inplace=True)
+    # change the column value that is NaN to 0 (ex. Q12: Nan -> Q12: 0)
+    df[col_name].replace(pd.unique(df[df[col_name].isnull()][col_name]), 0, inplace=True)
+
+
+def onehotEncoder(df, columns_to_encode):    
+    # Apply one-hot encoding to each column in the list
+    for column in columns_to_encode:
+        # Check if the column exists in the dataframe
+        if column in df.columns:
+            # Get one-hot encoding for the column
+            encoded_columns = pd.get_dummies(df[column], prefix=column, dtype=int)
+            # Drop the original column from the dataframe
+            df = df.drop(column, axis=1)
+            # Concatenate the new encoded columns to the original dataframe
+            df = pd.concat([df, encoded_columns], axis=1)
+        else:
+            print(f"Column {column} not found in the dataframe.")
+    
+    return df
---
+++ src/cleaning/data_encoding_constant.py
@@ -0,0 +1,12 @@
+"""
+Author:     Liao Yueh-Fan
+Data:       2023/11/8
+Course:     NUS IT5006 Fundamentals of Data Analytics
+Project:    Job Role Recommendation App
+Function:   Constant or Global Variables fot Data Encoding
+"""
+mulSelQuestionTitle = ["Q6_DS course platform", "Q12_coding language", "Q13_IDE", "Q14_notebook",
+                       "Q15_data visualization lib", "Q17_ML framework", "Q18_ML algo", "Q19_computer vision method", "Q20_NLP method"]
+
+
+sinSelQuestionTitle = ["Q3_gender", "Q8_academic dgree", 'Q4_country']
