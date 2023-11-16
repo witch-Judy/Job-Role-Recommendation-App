c68347fcfc8a947f7f0892fb19e9c0cc743d35f7
Yueh-Fan <gjm826f8@gmail.com>
2023-11-16 17:01:01 +0800

    Add Observations to Exploratory Data Analysis and redesign the webpage UI (Milestone 1)  Changes to be committed: 	modified:   app/visualize1.py 	new file:   docs/Exploratory data analysis disclaimer.csv 	new file:   docs/Exploratory data analysis observations.csv 	modified:   src/analysis/age_industry.py 	modified:   src/analysis/coding_language_country.py 	modified:   src/analysis/experience_vs_salary.py 	modified:   src/analysis/gender_distribution.py

--- app/visualize1.py
+++ app/visualize1.py
@@ -26,10 +26,12 @@ from src.analysis.gender_distribution import plot_gender_distribution
 def vis_income():
     list_of_top10 = ['India', 'United States of America', 'Japan', 'Brazil', 'Russia',
                      'United Kingdom of Great Britain and Northern Ireland', 'Nigeria', 'Spain', 'China', 'Germany']
+    st.header("Estimated Income Distribution in selected countries within the chosen timeframe")
 
-    country = st.multiselect('Select countries to display their median income distribution:', list_of_top10,
+    col1, col2 = st.columns([3, 2])
+    country = col1.multiselect('Select countries to display their median income distribution:', list_of_top10,
                              default='United States of America')
-    start, end = st.slider("Select the time frame", 2020, 2022, (2020, 2022))
+    start, end = col1.slider("Select the time frame", 2020, 2022, (2020, 2022))
 
     # read country_income data from analyzed folder
     income = pd.read_csv('./data/analyzed/country_income.csv', low_memory=False)
@@ -42,21 +44,15 @@ def vis_income():
     income = income.sort_values(by=['Country'])
     income.rename(columns={'Median': 'Median Income'}, inplace=True)
 
-    st.markdown("### Estimated Income Distribution in selected countries within the chosen timeframe ###")
-
     plt.figure(figsize=(10, 6))
     sns.set(style="whitegrid")
     ax = sns.boxplot(x='Country', y='Median Income', data=income)
     plt.title("Income Distribution by Country")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right', fontsize=10)
-    st.pyplot(plt)
+    col1.pyplot(plt)
 
-    # disclaimer
-    st.empty()
-    st.write('Disclaimer')
-    st.write('1. Top ten countries with the most respondants are selected.')
-    st.write('2. The income of each respondant is estimated as the average of the min and max of the chosen range.')
-    st.write('3. The outliers who earn more than $500,000 is filtered out.')
+    # observation and disclaimer
+    show_description(col2, "Income by Country")
 
     
 def vis_ML():
@@ -64,7 +60,7 @@ def vis_ML():
     pop_framework = pd.read_csv('./data/analyzed/framework.csv', low_memory=False)
     print(pop_framework)
 
-    st.markdown("### Change in the Composition of Popular Machine Learning Frameworks over the three years ###")
+    st.header("Change in the Composition of Popular Machine Learning Frameworks over the three years")
 
     # Convert absolute numbers to percentages
     for year in ['2020', '2021', '2022']:
@@ -87,26 +83,26 @@ def vis_ML():
         plt.xlabel('Frameworks')
         return fig
 
+    col1, col2 = st.columns([3, 2])
     # Create a percentage bar chart
-    st.pyplot(create_percentage_bar_chart(pop_framework))
+    col1.pyplot(create_percentage_bar_chart(pop_framework))
 
-    # disclaimer
-    st.write('Disclaimer')
-    st.write('1. Top 6 most used Machine Learning frameworks are shown and the rest are combined as Others')
+    # observation and disclaimer
+    show_description(col2, "Popular Machine Learning Frameworks")
 
 
 
 def vis_Jobtitle():
     # read job title data from analyzed folder
     title_of_top5 = pd.read_csv('./data/analyzed/job_title.csv', low_memory=False)
+    st.header("Top Five Job Titles of the respondants")
 
-    st.markdown("### Top Five Job Titles of the respondants ###")
-
+    col1, col2 = st.columns([3, 2])
     # Create a multiselect widget with the top 5 job titles
-    selected_titles = st.multiselect('Select job titles to display their trends:', title_of_top5['Title'], 'Student')
+    selected_titles = col1.multiselect('Select job titles to display their trends:', title_of_top5['Title'], 'Student')
 
     if selected_titles == []:
-        st.markdown(''':red[Please select at least one job title]''')
+        col1.markdown(''':red[Please select at least one job title]''')
 
     else:
         # Filter the DataFrame for the selected title
@@ -125,76 +121,94 @@ def vis_Jobtitle():
         ax.legend(title='Job Title')
 
         # Display the Matplotlib plot in Streamlit
-        st.pyplot(fig)
+        col1.pyplot(fig)
 
-    # disclaimer
-    st.write('Disclaimer')
-    st.write('1. Top five job titles of the respondants are selected')
-    st.write('2. The job titles of the same role are synchronized')
-
+    # observation and disclaimer
+    show_description(col2, "Popular Job Titles")
 
 def program_lan():
-    st.markdown("### The Trend of Programming Language used by Specific Country ###")
+    st.header("The Trend of Programming Language used by Specific Country")
     label_country = ['United States of America', 'Japan', 'United Kingdom of Great Britain and Northern Ireland',
                      'France', 'Germany', 'India', 'Brazil', 'China', 'Russia', 'Indonesia']
 
-    country = st.selectbox("Which country do you want to see?", label_country, 0)
-    plot_language_trend(country)
-    # disclaimer
-    st.write('Disclaimer')
-    st.write(
-        '1.Top 10 countries are selected, based on the number of responders in the dataset, we give you the top five developed and developing countries with the highest respective numbers.')
-    st.write(
-        "2.It is interesting to see that the largest number of people use Python, because the data set comes from the questionnaire sent to users by Kaggle, and most people who use Kaggle do machine learning and data analysis. Python is usually used to handle this kind of problem. ")
+    col1, col2 = st.columns([3, 2])
+    country = col1.selectbox("Which country do you want to see?", label_country, 0)
+    plot_language_trend(col1, country)
 
+    # observation and disclaimer
+    show_description(col2, "Popular Programming Languages")
 
 def age_dist():
-    st.markdown("### Industry Distribution for Age Range ###")
+    st.header("Industry Distribution for Age Range")
+
+    col1, col2 = st.columns([3, 2])
     label_ages = ["18-21", "22-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-69", "70+"]
-    ages = st.selectbox("which age range do you want to see? ", label_ages, 0)
+    ages = col1.selectbox("which age range do you want to see? ", label_ages, 0)
     label_years = [2021, 2022, (2021, 2022)]
-    years = st.selectbox("Which years range do you want to see?", label_years, 0)
+    years = col1.selectbox("Which years range do you want to see?", label_years, 0)
     if isinstance(years, int):
         years = [years]
-    plot_industry_distribution(ages, years)
-    st.write('Disclaimer')
-    st.write("There is no industry data in 2020, so we don't give you this option.")
+    plot_industry_distribution(col1, ages, years)
+
+    # observation and disclaimer
+    show_description(col2, "Industry Distribution on Different Age Ranges")
 
 
 def salary_exper():
-    st.markdown("### Relationship Between Coding Experience and Yearly Compensation. ###")
-    selected_years_range = st.slider('Select a range of years', 2020, 2022, (2020, 2022))
+    st.header("Relationship Between Coding Experience and Yearly Compensation.")
+
+    col1, col2 = st.columns([3, 2])
+    selected_years_range = col1.slider('Select a range of years', 2020, 2022, (2020, 2022))
     if selected_years_range[0] == selected_years_range[1]:
         years_list = [selected_years_range[0]]
     else:
         years_list = list(selected_years_range)
     years2 = list(years_list)
-    plot_experience_vs_compensation(years2)
-    st.write('Disclaimer')
-    st.write(
-        "In the questionnaire survey data set, there are no 1-2 year options for 2021 and 2022, so they are empty. ")
+    plot_experience_vs_compensation(col1, years2)
+    # observation and disclaimer
+    show_description(col2, "Experience vs Salary")
 
 
 def gen_dist():
-    st.markdown("### the gender percentage of different countries. ###")
+    st.header("Gender Percentage of Different Countries")
     # label_years3 = [(2020, 2021, 2022), 2020, 2021, 2022, (2020, 2021), (2020, 2022), (2021, 2022)]
     # years3 = st.selectbox("Which years range do you want to see?", label_years3, 0)
     # if isinstance(years3, int):
     #     years3 = [years3]
 
-    selected_years_range2 = st.slider('Select a range of years', 2020, 2022, (2020, 2022))
+    col1, col2 = st.columns([3, 2])
+    selected_years_range2 = col1.slider('Select a range of years', 2020, 2022, (2020, 2022))
     if selected_years_range2[0] == selected_years_range2[1]:
         years_list2 = [selected_years_range2[0]]
     else:
         years_list2 = list(selected_years_range2)
     years3 = list(years_list2)
-    plot_gender_distribution(years3)
-    st.write('Disclaimer')
-    st.write(
-        "Top 10 countries are selected, based on the number of responders in the dataset, we give you the top five developed and developing countries with the highest respective numbers.")
+    plot_gender_distribution(col1, years3)# observation and disclaimer
+    show_description(col2, "Gender Distribution")
 
+def show_description(col, title):
+    col.empty()
+    df_observation = pd.read_csv("./docs/Exploratory data analysis observations.csv")
+    df_disclaimer = pd.read_csv("./docs/Exploratory data analysis disclaimer.csv")
+    doc_list = [df_observation, df_disclaimer]
+    doc_header = ["Observation", "Disclaimer"]
+    # loop all the content(observation/disclaimer) in that column
+    for idx in range(len(doc_header)):
+        row = 0
+        col.subheader(doc_header[idx])
+        while True:
+            if row == doc_list[idx].shape[0]:
+                break
+            elif pd.notna(doc_list[idx].loc[row, title]):
+                # print observation
+                obs_str = str(row+1) + ". " + doc_list[idx].loc[row, title]
+                col.write(obs_str)
+            else:
+                break
+            row += 1
 
 import streamlit as st
+st.set_page_config(layout="wide")
 
 st.sidebar.title("Job-Role-Recommendation-App")
 
@@ -204,14 +218,14 @@ if main_selection == "analysis":
     analysis_selection = st.sidebar.selectbox("Choose an analysis:", ["Trends", "Statistics"])
 
     if analysis_selection == "Trends":
-        labelx = ['Popular Job Titles', 'Popular Machine Learning Frameworks', 'Popular Programming Language']
+        labelx = ['Popular Job Titles', 'Popular Machine Learning Frameworks', 'Popular Programming Languages']
         x = st.sidebar.selectbox("Which trend do you want to see?", labelx, 0)
 
         if x == 'Popular Machine Learning Frameworks':
             vis_ML()
         elif x == 'Popular Job Titles':
             vis_Jobtitle()
-        elif x == 'Popular Programming Language':
+        elif x == 'Popular Programming Languages':
             program_lan()
 
     elif analysis_selection == "Statistics":
